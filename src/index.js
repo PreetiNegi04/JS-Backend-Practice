fe21aa806dd0cc36f2f169081f6dac03ae38921d
Preeti Negi <negipreeti2003@gmail.com>
2026-09-26 13:15:43 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log("Server is running on PORT: ", process.env.PORT)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECTION FAILED!!")
+})
